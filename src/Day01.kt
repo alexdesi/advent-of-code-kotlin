8138d4a36cd8ba32ed91acdaafc1d07b32eb0ab3
Alexdesi <alexdesi@gmail.com>
2022-12-07 10:53:55 +0000

    Day 1: Calorie Counting
    Part 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
    Part 2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
+import java.io.File
+
 fun main() {
+    fun sortedCalories(input: List<String>): List<Int> {
+        val elvesCalories = mutableListOf(
+            mutableListOf<Int>()
+        )
+
+        input.forEach {
+            if (it == "")
+                elvesCalories.add(mutableListOf())
+            else
+                elvesCalories.last().add(it.toInt())
+        }
+
+        return elvesCalories
+            .map { it.sum() }
+            .sortedDescending()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return sortedCalories(input).first()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return sortedCalories(input)
+            .subList(0, 3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+
